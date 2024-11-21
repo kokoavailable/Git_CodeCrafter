f9a5a9582aae36e4635b2fe3aa6cf8c9539cd1b9
Monsangter <sangdobae@icloud.com>
2024-11-21 20:08:39 +0900

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import zlib
+import hashlib
 
 def init_git():
     os.mkdir(".git")
@@ -29,6 +30,28 @@ def cat_file(blob_sha):
     content = decompressed_data[header_end + 1:]
     print(content.decode(), end= "")
 
+def hash_object(file_path, write=False):
+    with open(file_path, "rb") as f:
+        content = f.read()
+
+    size = len(content)
+    blob = f"blob {size}\0".encode() + content
+
+    sha1 = hashlib.sha1(blob).hexdigest()
+
+    if write:
+
+        object_dir = f".git/objects/{sha1[:2]}"
+        object_file = f"{object_dir}/{sha1[2:]}"
+        if not os.path.exists(object_dir):
+            os.makedirs(object_dir)
+
+        compressed_blob = zlib.compress(blob)
+
+        with open(object_file, "wb") as f:
+            f.write(compressed_blob)
+
+    print(sha1)
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -42,6 +65,9 @@ def main():
     elif command == "cat-file" and sys.argv[2] == "-p":
         blob_sha = sys.argv[3]
         cat_file(blob_sha)
+    elif command == "hash-object" and sys.argv[2] == "-w":
+        file_path = sys.argv[3]
+        hash_object(file_path, write=True)
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
